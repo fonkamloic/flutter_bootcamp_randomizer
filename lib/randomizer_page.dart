3045b2096812725532750baabb97463a08fbc99a
Fonkam Loïc <fonkamloic@gmail.com>
2021-09-30 23:11:30 -0400

    ./lib/randomizer_page

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,29 +1,31 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_foundations/randomizer_change_notifier.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/src/provider.dart';
 
-class RandomizerPage extends HookWidget {
-  RandomizerPage({Key? key, required this.max, required this.min})
-      : super(key: key);
-  final int min, max;
+class RandomizerPage extends StatelessWidget {
+  const RandomizerPage({
+    Key? key,
+  }) : super(key: key);
 
-  final randomGenerator = Random();
   @override
   Widget build(BuildContext context) {
-    final _generatedNumber = useState<int?>(null);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
       body: Center(
-        child: Text(_generatedNumber.value?.toString() ?? 'Generate a number',
-            style: const TextStyle(fontSize: 42)),
+        child: Consumer<RandomizerChangeNotifier>(
+          builder: (context, notifier, child) => Text(
+              notifier.generatedNumber?.toString() ?? 'Generate a number',
+              style: const TextStyle(fontSize: 42)),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          _generatedNumber.value = min + randomGenerator.nextInt(max + 1 - min);
-        },
+        onPressed: () =>
+            context.read<RandomizerChangeNotifier>().generateRandomNumber(),
         label: const Text('Generate'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
