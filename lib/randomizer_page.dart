c94d341085ef4fc8ce852b7c76b16155239797ac
Fonkam Loïc <fonkamloic@gmail.com>
2021-10-01 01:07:23 -0400

    ./lib/randomizer_page

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,31 +1,34 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_foundations/main.dart';
 import 'package:flutter_foundations/randomizer_change_notifier.dart';
-import 'package:provider/provider.dart';
-import 'package:provider/src/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RandomizerPage extends StatelessWidget {
+class RandomizerPage extends ConsumerWidget{
   const RandomizerPage({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ScopedReader ref) {
+            final randomizer = ref(randomizerProvider) ;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Randomizer'),
       ),
       body: Center(
-        child: Consumer<RandomizerChangeNotifier>(
-          builder: (context, notifier, child) => Text(
-              notifier.generatedNumber?.toString() ?? 'Generate a number',
-              style: const TextStyle(fontSize: 42)),
+        child: Consumer(
+          builder: (context, ref, child) {
+            return Text(
+            randomizer  .generatedNumber?.toString() ?? 'Generate a number',
+              style: const TextStyle(fontSize: 42));
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () =>
-            context.read<RandomizerChangeNotifier>().generateRandomNumber(),
+            randomizer.generateRandomNumber(),
         label: const Text('Generate'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
