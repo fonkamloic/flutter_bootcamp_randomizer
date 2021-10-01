d04df665d16860bee072e342aab403be7508c6b1
Fonkam Loïc <fonkamloic@gmail.com>
2021-09-30 22:09:46 -0400

    ./lib/randomizer_page

---
+++ lib/randomizer_page.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class RandomizerPage extends HookWidget {
+  RandomizerPage({Key? key, required this.max, required this.min})
+      : super(key: key);
+  final int min, max;
+
+  final randomGenerator = Random();
+  @override
+  Widget build(BuildContext context) {
+    final _generatedNumber = useState<int?>(null);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Randomizer'),
+      ),
+      body: Center(
+        child: Text(_generatedNumber.value?.toString() ?? 'Generate a number',
+            style: const TextStyle(fontSize: 42)),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          _generatedNumber.value = min + randomGenerator.nextInt(max + 1 - min);
+        },
+        label: const Text('Generate'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
