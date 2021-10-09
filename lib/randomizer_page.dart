7617d5c5457bbd03ad84ef0dd4a87c4a8c60de24
Fonkam Loïc <fonkamloic@gmail.com>
2021-10-08 22:45:44 -0400

    ./lib/randomizer_page

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,25 +1,14 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_foundations/bloc/randomizer_bloc.dart';
 import 'package:flutter_foundations/main.dart';
 import 'package:flutter_foundations/randomizer_state_notifier.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class RandomizerPage extends StatefulWidget {
-  final int min, max;
+class RandomizerPage extends StatelessWidget {
   const RandomizerPage({
     Key? key,
-    required this.min,
-    required this.max,
   }) : super(key: key);
 
-  @override
-  State<RandomizerPage> createState() => _RandomizerPageState();
-}
-
-class _RandomizerPageState extends State<RandomizerPage> {
-  int? _generatedNumber;
-  final randomGenerator = Random();
   @override
   Widget build(BuildContext context) {
     // final randomizer = ref(randomizerProvider);
@@ -28,19 +17,17 @@ class _RandomizerPageState extends State<RandomizerPage> {
         title: const Text('Randomizer'),
       ),
       body: Center(
-        child: Consumer(
-          builder: (context, ref, child) {
-            return Text(_generatedNumber?.toString() ?? 'Generate a number',
+        child: BlocBuilder<RandomizerBloc, RandomizerState>(
+          builder: (context, state) {
+            return Text(
+                state.generatedNumber?.toString() ?? 'Generate a number',
                 style: const TextStyle(fontSize: 42));
           },
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          setState(() {
-            _generatedNumber = widget.min +
-                randomGenerator.nextInt(widget.max + 1 - widget.min);
-          });
+          context.read<RandomizerBloc>().add(const RandomizerEvent.generate());
         },
         label: const Text('Generate'),
       ),
